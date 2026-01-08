7f58214b88439457bf0bacbf94460ec031a37f76
BLuLaptop <brendannlu5@gmail.com>
2026-01-08 18:43:26 +1100

    tests: add test for tsvdm matrix singular values output

--- tests/testthat/test-tens.tsvdm.R
+++ tests/testthat/test-tens.tsvdm.R
@@ -20,7 +20,7 @@ test_that(
 )
 
 test_that(
-  "reconstructing original matrix from tsvdm without transform",
+  "reconstructing original matrix from tsvdm when transform=FALSE",
   code = {
     test_tensor <- array(1:24, dim = c(2, 4, 3))
     tsvdm_decomposition <- tsvdm(test_tensor, transform = FALSE)
@@ -33,5 +33,23 @@ test_that(
       test_tensor,
       facewise_product(u, s, vt)
     )
+  }
+)
+
+test_that(
+  "`svals_matrix_form = TRUE` gives consistent output",
+  code = {
+    test_tensor <- array(1:24, dim = c(2, 4, 3))
+
+    tsvdm_decomposition_tensor <- tsvdm(test_tensor)
+    s_tensor <- tsvdm_decomposition_tensor$s
+    tsvdm_decomposition_matrix <- tsvdm(test_tensor, svals_matrix_form = TRUE)
+    s_matrix <- tsvdm_decomposition_matrix$s
+
+    s_tensor_reconstruct <- array(0, dim = dim(s_tensor))
+    for (i in seq_len(dim(s_tensor)[3])) {
+      s_tensor_reconstruct[, , i] <- diag(s_matrix[, i])
+    }
+    expect_equal(s_tensor, s_tensor_reconstruct)
   }
 )
